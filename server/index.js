242db7d1768e8ee0a58146da7464d6daeb2df3c8
dev0xgenius <dev0xgenius@gmail.com>
2025-07-19 21:33:22 +0100

    [update] Update DB settings & rename db/index.js -> db.js

--- server/index.js
+++ server/index.js
@@ -1,14 +1,75 @@
-const { getComments } = require("./db");
+const db = require("./db");
 const express = require("express");
 const { ChatServer } = require("./lib/ChatServer");
+const e = require("express");
 
 const app = express();
-const chat = new ChatServer(app, "ws://localhost:8080", "/comments");
-
-getComments().then((comments) => (chat.messages = comments));
-
-chat.init((httpServer) => {
-  httpServer.listen(8080, () => {
-    console.log("Running on port: 8080");
-  });
+const chat = new ChatServer({
+  httpServer: app,
+  baseUrl: "ws://localhost:8080",
+  path: "/comments",
 });
+
+app.use(express.static("../client/dist"));
+
+async function resolveComment(comment) {
+  try {
+    const user = await db.getUser(comment.user_id);
+
+    comment = {
+      ...comment,
+      user: {
+        image: { png: user.image_url, webp: "" },
+        username: user.username,
+      },
+      createdAt: new Date(comment.created_at).getTime(),
+    };
+
+    if ("replying_to" in comment) {
+      let username = await db.query(
+        `SELECT username FROM users WHERE id=
+          (SELECT user_id FROM comments WHERE id=$1)`,
+        [comment.replying_to],
+      ).rows[0][0];
+
+      comment.replyingTo = username;
+    } else {
+      let replies = await db.getReplies(comment.id);
+      comment.replies = await Promise.all(
+        replies.map((reply) => resolveComment(reply)),
+      );
+    }
+  } catch (e) {
+    throw new Error(e);
+  } finally {
+    delete comment.replying_to;
+    delete comment.user_id;
+    delete comment.created_at;
+
+    return comment;
+  }
+}
+
+function resolveChatMessages() {
+  return new Promise((resolve, reject) => {
+    db.getComments().then((comments) => {
+      let resolvedComments = Promise.all(
+        comments.map((comment) => resolveComment(comment)),
+      );
+
+      resolvedComments.then(resolve).catch(reject);
+    });
+  });
+}
+
+resolveChatMessages()
+  .then((messages) => {
+    chat.messages = messages;
+
+    chat.init((httpServer) => {
+      httpServer.listen(8080, () => {
+        console.log("Running on port: 8080");
+      });
+    });
+  })
+  .catch(console.log);
