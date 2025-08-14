05827b7f784971b1397399e6db91793b0121be6d
dev0xgenius <dev0xgenius@gmail.com>
2025-08-14 03:28:46 +0100

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,8 @@
+const path = require("path");
 const uuid = require("uuid");
 const { Pool } = require("pg");
-require("dotenv").config({ path: "../.env" });
+
+require("dotenv").config({ path: path.join(__dirname, "../../.env") });
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
