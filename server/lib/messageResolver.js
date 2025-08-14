1a1c0f49d1afc997d42b176e626320f8698b019b
dev0xgenius <dev0xgenius@gmail.com>
2025-08-14 02:47:26 +0100

    Make minor updates

--- server/lib/messageResolver.js
+++ server/lib/messageResolver.js
@@ -1,87 +1,87 @@
 const db = require("../db");
 const {
-  addComment,
-  addReply,
-  deleteReply,
-  editReply,
+    addComment,
+    addReply,
+    deleteReply,
+    editReply,
 } = require("../db/queries");
 
 async function resolveComment(comment) {
-  try {
-    const user = await db.getUser(comment.user_id);
+    try {
+        const user = await db.getUser(comment.user_id);
 
-    comment = {
-      ...comment,
-      user: {
-        image: { png: user.image_url, webp: "" },
-        username: user.username,
-      },
-      createdAt: new Date(comment.created_at).getTime(),
-    };
+        comment = {
+            ...comment,
+            user: {
+                image: { png: user.image_url, webp: "" },
+                username: user.username,
+            },
+            createdAt: new Date(comment.created_at).getTime(),
+        };
 
-    if ("comment_id" in comment) {
-      comment.commentID = comment.comment_id;
-      comment.replyingTo = comment.replying_to;
-    } else {
-      let replies = await db.getReplies(comment.id);
-      comment.replies = await Promise.all(
-        replies.map((reply) => resolveComment(reply)),
-      );
+        if ("comment_id" in comment) {
+            comment.commentID = comment.comment_id;
+            comment.replyingTo = comment.replying_to;
+        } else {
+            let replies = await db.getReplies(comment.id);
+            comment.replies = await Promise.all(
+                replies.map((reply) => resolveComment(reply)),
+            );
+        }
+    } catch (e) {
+        throw new Error(e);
+    } finally {
+        delete comment.replying_to;
+        delete comment.user_id;
+        delete comment.created_at;
+        delete comment.comment_id;
+
+        return comment;
     }
-  } catch (e) {
-    throw new Error(e);
-  } finally {
-    delete comment.replying_to;
-    delete comment.user_id;
-    delete comment.created_at;
-    delete comment.comment_id;
-
-    return comment;
-  }
 }
 
 function messageResolver(message) {
-  message = JSON.parse(message);
+    message = JSON.parse(message);
 
-  switch (message.type) {
-    case "ADD_COMMENT":
-      return new Promise((resolve, reject) => {
-        addComment(message.data)
-          .then(resolveComment)
-          .then((data) => {
-            message.data = data;
-            resolve(JSON.stringify(message));
-          })
-          .catch(reject);
-      });
-    case "ADD_REPLY":
-      return new Promise((resolve, reject) => {
-        addReply(message.data)
-          .then(resolveComment)
-          .then((data) => {
-            message.data = data;
-            resolve(JSON.stringify(message));
-          })
-          .catch(reject);
-      });
-    case "DELETE_REPLY":
-      return new Promise((resolve, reject) => {
-        deleteReply(message.data).catch(reject);
-        resolve(JSON.stringify(message));
-      });
-    case "EDIT_REPLY":
-      return new Promise((resolve, reject) => [
-        editReply(message.data)
-          .then(resolveComment)
-          .then((data) => {
-            message.data = data;
-            resolve(JSON.stringify(message));
-          }),
-      ]);
-  }
+    switch (message.type) {
+        case "ADD_COMMENT":
+            return new Promise((resolve, reject) => {
+                addComment(message.data)
+                    .then(resolveComment)
+                    .then((data) => {
+                        message.data = data;
+                        resolve(JSON.stringify(message));
+                    })
+                    .catch(reject);
+            });
+        case "ADD_REPLY":
+            return new Promise((resolve, reject) => {
+                addReply(message.data)
+                    .then(resolveComment)
+                    .then((data) => {
+                        message.data = data;
+                        resolve(JSON.stringify(message));
+                    })
+                    .catch(reject);
+            });
+        case "DELETE_REPLY":
+            return new Promise((resolve, reject) => {
+                deleteReply(message.data).catch(reject);
+                resolve(JSON.stringify(message));
+            });
+        case "EDIT_REPLY":
+            return new Promise((resolve, reject) => [
+                editReply(message.data)
+                    .then(resolveComment)
+                    .then((data) => {
+                        message.data = data;
+                        resolve(JSON.stringify(message));
+                    }),
+            ]);
+    }
 }
 
 module.exports = {
-  messageResolver,
-  resolveComment,
+    messageResolver,
+    resolveComment,
 };
