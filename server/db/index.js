992cd7e6633b6e5710d2ab8c4ec13ecd3e86e874
dev0xgenius <dev0xgenius@gmail.com>
2025-08-14 03:23:16 +0100

    [fix] Update dotenv call

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const uuid = require("uuid");
 const { Pool } = require("pg");
-require("dotenv").config();
+require("dotenv").config({ path: "../.env" });
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
